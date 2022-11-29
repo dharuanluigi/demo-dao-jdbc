890c473351268ce9f5e8e7fcddcfbda513deaa7b
Dharuan Luigi <dharuanluigi@gmail.com>
2022-11-29 17:02:14 -0300

    Seller entitie class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		var dep = new Department(1, "Books");
+		var seller = new Seller(1, "Dharuan", "dharuan@email.com", new Date(), 3000.0, dep);
 		System.out.println(dep);
+		System.out.println(seller);
 	}
 
 }
